57e0baef862f0868a35e78a62c468e40b22080de
Reza Shirazian <reza.shirazian@gmail.com>
2016-04-07 00:02:07 -0700

    Initial Commit

---
+++ Mechanic - Chain Of Responsibility/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Mechanic - Chain Of Responsibility
+//
+//  Created by Reza Shirazian on 2016-04-07.
+//  Copyright © 2016 Reza Shirazian. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
